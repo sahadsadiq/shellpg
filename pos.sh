08c2c9e45477eca5deb6c94897641dd085243f5a
sahadsadiq <sahadsadhiq007@gmail.com>
2019-05-16 16:50:07 +0530

    fourthcommit

---
+++ pos.sh
@@ -0,0 +1,11 @@
+   echo -n "Enter the first number : "
+    read num1
+    if [ $num1  -gt 0 ]
+then
+echo "number $num1 is + ve"
+elif [ $num1 -lt 0 ]
+then
+echo "number $num1 is - ve"
+else
+echo " moonchi"
+fi
